0b5df977d6e48833118199101fe8a43f061ef00b
Linxl <673615950@qq.com>
2018-05-25 22:13:06 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/linxl/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/linxl/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.linxl.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -232,5 +233,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
